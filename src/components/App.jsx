ab752ca0bb89b7db9c5b4a488a59d830614f4f0d
Chivu Andrei <chivuandrei2003@gmail.com>
2025-09-29 00:41:58 +0300

    add assets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 //import { useState } from 'react'
+import { Header } from './header'
+import '../App.css'
 
-import './App.css'
-
-function App() {
+export function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <p>Hello world</p>
-      </div>
+      <Header />
         
     </>
   )
